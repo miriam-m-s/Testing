4ae97d4cd906382d2ed2cc789866f6b0d03ecbdc
JosedaMachine <jrave@ucm.es>
2022-11-30 14:28:44 +0100

    Practica 2

---
+++ P2_CriteriosCobertura/Agenda/src/Agenda/DatoException.java
@@ -0,0 +1,11 @@
+package Agenda;
+
+public class DatoException extends Exception {
+	
+private static final long serialVersionUID = 1L;
+
+	public DatoException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ P2_CriteriosCobertura/Agenda/src/AgendaBreak/DatoException.java
@@ -0,0 +1,11 @@
+package AgendaBreak;
+
+public class DatoException extends Exception {
+	
+private static final long serialVersionUID = 1L;
+
+	public DatoException(String msg) {
+		super(msg);
+	}
+
+}
